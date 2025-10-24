f3e42de9f334c5a2f6b1319e0c9407a00a3349e9
dubratati987 <84503134+dubratati987@users.noreply.github.com>
2025-10-24 21:47:08 +0530

    in-progress: Implement security feature with arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRouter from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(
     stream: { write: message => logger.info(message.trim()) },
   })
 );
+// app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from acquisition');
